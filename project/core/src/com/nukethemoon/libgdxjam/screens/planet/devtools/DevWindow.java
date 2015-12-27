72a32f1f7f7d36e1cf3dd26dc81cd4d5c5cd8321
Luca Hofmann <HofmannLuca@gmail.com>
2015-12-27 22:50:08 +0100

    Added ambient light editor.

--- project/core/src/com/nukethemoon/libgdxjam/screens/planet/devtools/DevWindow.java
+++ project/core/src/com/nukethemoon/libgdxjam/screens/planet/devtools/DevWindow.java
@@ -1,6 +1,7 @@
 package com.nukethemoon.libgdxjam.screens.planet.devtools;
 
 
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -22,6 +23,15 @@ public class DevWindow extends Window {
 		});
 		getTitleTable().add(closeButton).height(18);
 
+		ColorAttribute colorAttribute = planetConfig.environmentColorAttributes.get(0);
+		ColorForm cf = new ColorForm(skin, colorAttribute.color, new ColorForm.ColorChangeListener() {
+			@Override
+			public void onColorChange(float v, float v1, float v2) {
+				pack();
+			}
+		});
+		add(cf);
+		row();
 
 		for (DirectionalLight dLight : planetConfig.environmentDirectionalLights) {
 			ColorForm colorForm = new ColorForm(skin, dLight.color, new ColorForm.ColorChangeListener() {
