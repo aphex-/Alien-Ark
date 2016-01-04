6cd14fe5347d749035b85897828f667b37c4381b
Luca Hofmann <HofmannLuca@gmail.com>
2016-01-04 23:05:35 +0100

    added explosion

--- project/core/src/com/nukethemoon/libgdxjam/ui/GameOverTable.java
+++ project/core/src/com/nukethemoon/libgdxjam/ui/GameOverTable.java
@@ -11,12 +11,9 @@ public class GameOverTable extends CenteredTable {
 
 	public GameOverTable(Skin skin, final ClickListener clickListener) {
 		setBackground(Styles.NINE_PATCH_POPUP_BG_01);
-
 		add(new Label("GAME OVER", Styles.LABEL_01));
 		row();
-
 		TextButton okButton = new TextButton("OK", Styles.STYLE_BUTTON_01);
-
 		okButton.addListener(clickListener);
 		add(okButton);
 		pad(20);
