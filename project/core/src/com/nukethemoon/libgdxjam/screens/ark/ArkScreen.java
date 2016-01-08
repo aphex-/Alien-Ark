a64f2835fabcf0f003fd967641245ca470ad0763
lovis <lovis.moeller@appdream.de>
2016-01-08 16:51:46 +0100

    some cleanup and aliens inventory

--- project/core/src/com/nukethemoon/libgdxjam/screens/ark/ArkScreen.java
+++ project/core/src/com/nukethemoon/libgdxjam/screens/ark/ArkScreen.java
@@ -3,7 +3,6 @@ package com.nukethemoon.libgdxjam.screens.ark;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -11,26 +10,24 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.nukethemoon.libgdxjam.App;
-import com.nukethemoon.libgdxjam.Log;
-import com.nukethemoon.libgdxjam.Styles;
+import com.nukethemoon.libgdxjam.game.Alien;
 import com.nukethemoon.libgdxjam.game.Artifact;
 import com.nukethemoon.libgdxjam.game.SpaceShipProperties;
-import com.nukethemoon.libgdxjam.game.artifacts.ValueArtifact;
-import com.nukethemoon.libgdxjam.ui.CenteredTable;
 
 import java.util.List;
 
 public class ArkScreen implements Screen {
 
+	public static final int INVENTORY_WIDTH = 245;
+	public static final int INVENTORY_OFFSET_X = 75;
+	public static final int INVENTORY_HEIGHT = 355;
+	public static final int INVENTORY_Y = 200;
 	private WorkbenchSlot workbenchSlot1;
 	private WorkbenchSlot workbenchSlot2;
 	private WorkbenchSlot workbenchSlot3;
@@ -45,6 +42,8 @@ public class ArkScreen implements Screen {
 
 	private Stage stage;
 	private DragAndDrop dragAndDrop;
+	private Table alienTable;
+	private Table artifactsTable;
 
 	public ArkScreen(Skin uiSkin, InputMultiplexer multiplexer) {
 		skin = uiSkin;
@@ -59,25 +58,71 @@ public class ArkScreen implements Screen {
 	@Override
 	public void show() {
 		workbenchSlot1 = new WorkbenchSlot();
-		workbenchSlot1.setBounds(400, 435, 75, 75);
-
+		int dimension = 75;
+		workbenchSlot1.setBounds(400, 435, dimension, dimension);
 
 		workbenchSlot2 = new WorkbenchSlot();
-		workbenchSlot2.setBounds(527, 435, 75, 75);
+		workbenchSlot2.setBounds(527, 435, dimension, dimension);
 
 		workbenchSlot3 = new WorkbenchSlot();
-		workbenchSlot3.setBounds(652, 435, 75, 75);
+		workbenchSlot3.setBounds(652, 435, dimension, dimension);
 
 		resultArea = new Actor();
-		resultArea.setBounds(777, 435, 75, 75);
+		resultArea.setBounds(777, 435, dimension, dimension);
 
 		background = new Texture("textures/background02.png");
 
-		final Table artifactsTable = new Table();
-		artifactsTable.setWidth(245);
+		createArtifactsInventory();
+		createAlienInventory();
+
+		updateArtifactsInventory();
+		updateAlienInventory();
+
+		addDropTarget(workbenchSlot1);
+		addDropTarget(workbenchSlot2);
+		addDropTarget(workbenchSlot3);
+
+
+		multiplexer.addProcessor(stage);
+
+	}
+
+	private void createArtifactsInventory() {
+		artifactsTable = new Table();
+		artifactsTable.setWidth(INVENTORY_WIDTH);
 		artifactsTable.pad(3);
 		artifactsTable.top();
+		ScrollPane artifactsScrollPane = new ScrollPane(artifactsTable);
+
+		artifactsScrollPane.setX(INVENTORY_OFFSET_X);
+		artifactsScrollPane.setY(INVENTORY_Y);
+		artifactsScrollPane.setWidth(INVENTORY_WIDTH);
+		artifactsScrollPane.setHeight(INVENTORY_HEIGHT);
+		artifactsScrollPane.setScrollingDisabled(true, false);
+
+		stage.addActor(artifactsScrollPane);
+	}
+
+	private void createAlienInventory() {
+		alienTable = new Table();
+		alienTable.setWidth(INVENTORY_WIDTH);
+		alienTable.pad(3);
+		alienTable.top();
+
+		ScrollPane alienScrollPane = new ScrollPane(alienTable);
+		alienScrollPane.setX(Gdx.graphics.getWidth() - INVENTORY_OFFSET_X - INVENTORY_WIDTH);
+		alienScrollPane.setY(INVENTORY_Y);
+		alienScrollPane.setWidth(INVENTORY_WIDTH);
+		alienScrollPane.setHeight(INVENTORY_HEIGHT);
+		alienScrollPane.setScrollingDisabled(true, false);
+
+		stage.addActor(alienScrollPane);
+	}
+
+	private void updateArtifactsInventory() {
+		artifactsTable.clear();
 		final List<Artifact> artifacts = SpaceShipProperties.properties.getArtifacts();
+
 		for (int i = 0; i < artifacts.size(); ++i) {
 			final Artifact artifact = artifacts.get(i);
 			final Image img = new Image(artifact.getTexture());
@@ -107,9 +152,9 @@ public class ArkScreen implements Screen {
 
 				@Override
 				public void dragStop(InputEvent event, float x, float y, int pointer, DragAndDrop.Payload payload, DragAndDrop.Target target) {
-					if(target != null) {
+					if (target != null) {
 						artifactsTable.removeActor(getActor());
-						artifacts.remove(payload.getObject());
+						//artifacts.remove(payload.getObject());
 					} else {
 						img.setVisible(true);
 					}
@@ -118,23 +163,21 @@ public class ArkScreen implements Screen {
 			if ((i + 1) % 3 == 0)
 				artifactsTable.row();
 		}
+	}
 
-		addDropTarget(workbenchSlot1);
-		addDropTarget(workbenchSlot2);
-		addDropTarget(workbenchSlot3);
+	private void updateAlienInventory() {
+		alienTable.clear();
+		final List<Alien> aliens = SpaceShipProperties.properties.getAliens();
 
-		ScrollPane scrollPane = new ScrollPane(artifactsTable);
+		for (int i = 0; i < aliens.size(); ++i) {
+			final Alien alien = aliens.get(i);
+			final Image img = new Image(App.TEXTURES.findRegion("placeholder_alien"));
 
-		scrollPane.setX(75);
-		scrollPane.setY(200);
-		scrollPane.setWidth(245);
-		scrollPane.setHeight(355);
-		scrollPane.setScrollingDisabled(true, false);
+			alienTable.add(img).space(5).width(75).height(75);
 
-		stage.addActor(scrollPane);
-		multiplexer.addProcessor(stage);
-
-
+			if ((i + 1) % 3 == 0)
+				alienTable.row();
+		}
 	}
 
 	private void addDropTarget(final WorkbenchSlot slot) {
@@ -153,7 +196,7 @@ public class ArkScreen implements Screen {
 			@Override
 			public void drop(DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
 				slot.insertArtifact((Artifact) payload.getObject());
-				slot.setDrawable(((Image)source.getActor()).getDrawable());
+				slot.setDrawable(((Image) source.getActor()).getDrawable());
 				dragAndDrop.removeTarget(this);
 
 			}
@@ -208,7 +251,7 @@ public class ArkScreen implements Screen {
 	private class WorkbenchSlot extends Image {
 		private Artifact insertedArtifact;
 
-		public WorkbenchSlot(){
+		public WorkbenchSlot() {
 			super(App.TEXTURES.findRegion("slotDrop"));
 		}
 
